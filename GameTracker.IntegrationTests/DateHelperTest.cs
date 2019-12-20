5bc64c483a3d203b305eeef3aed53c9de2896472
Niklas <niklas.thuer@gmail.com>
2019-12-20 10:58:25 +0100

    DateHelperTest

---
+++ GameTracker.IntegrationTests/DateHelperTest.cs
@@ -0,0 +1,60 @@
+﻿using FluentAssertions;
+using GameTracker.Models;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+using Xunit;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Text.Json;
+using System.Text;
+using Newtonsoft.Json;
+using System;
+using GameTracker.Helper;
+using Xunit.Extensions.Ordering;
+
+namespace GameTracker.IntegrationTests
+{
+    public class DateHelperTest
+    {
+        private DateHelper _dateHelper = DateHelper.getInstance();
+
+        [Fact]
+        public void CreateTodayDate_THEN_CheckBeforeToday_ResultsInFalse()
+        {
+            var today = DateTime.Today;
+            _dateHelper.checkBeforeToday(today);
+        }
+        
+        [Fact]
+        public void CreateOldDate_THEN_CheckBeforeToday_ResultsInTrue()
+        {
+            var today = DateTime.Today;
+            var checkDate = new DateTime(today.Year-1, today.Month, today.Day);
+            _dateHelper.checkBeforeToday(today);
+        }
+        
+        [Fact]
+        public void CreateTodayDate_THEN_CheckBeforeEqualsToday_ResultsInTrue()
+        {
+            var today = DateTime.Today;
+            _dateHelper.checkBeforeEqualsToday(today);
+        }
+        
+        [Fact]
+        public void CreateFutureDate_THEN_CheckBeforeToday_ResultsInFalse()
+        {
+            var today = DateTime.Today;
+            var checkDate = new DateTime(today.Year +1, today.Month, today.Day);
+            _dateHelper.checkBeforeEqualsToday(checkDate);
+        }
+        
+        [Fact]
+        public void CreateFutureDate_THEN_CheckBeforeEqualsToday_ResultsInFalse()
+        {
+            var today = DateTime.Today;
+            var checkDate = new DateTime(today.Year +1, today.Month, today.Day);
+            _dateHelper.checkBeforeEqualsToday(checkDate);
+        }
+    }
+}
